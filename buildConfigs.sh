d824ed36a355cdcc4ebe041b60b88227cea18937
Jessica Mulein <jessica@mulein.com>
2022-06-26 01:57:58 -0700

    address build issues

--- buildConfigs.sh
+++ buildConfigs.sh
@@ -10,8 +10,9 @@ echo "Running ${0} for project ${projectId} from ${currentDir}"
 
 
 echo "Making src/firebaseAppCheckConfig.ts"
-APP_CHECK_CONFIG="export const firebaseAppCheckConfig = {\\n  siteKey: '${firebaseAppCheckKey}',\\n};"
-echo -n -e "${APP_CHECK_CONFIG}" > ./src/firebaseAppCheckConfig.ts
+#APP_CHECK_CONFIG="export const firebaseAppCheckConfig = {\\n  siteKey: '${firebaseAppCheckKey}',\\n};"
+#echo -n -e "${APP_CHECK_CONFIG}" > ./src/firebaseAppCheckConfig.ts
+cat src/firebaseAppCheckConfig.ts.t | sed "s/__APP_CHECK_SITE_KEY__/${firebaseAppCheckKey}/" > src/firebaseAppCheckConfig.ts
 
 echo "Making .firebaserc"
 cat _firebaserc.t | sed "s/__FIREBASE_SITE__/${projectId}/" > .firebaserc
--- src/firebaseAppCheckConfig.ts.t
+++ src/firebaseAppCheckConfig.ts.t
@@ -3,5 +3,5 @@ import { IFirebaseAppCheckConfig } from './interfaces';
 /* for local development: please fill in the siteKey below and do not commit it */
 
 export const firebaseAppCheckConfig: IFirebaseAppCheckConfig = {
-  siteKey: '',
+  siteKey: '__APP_CHECK_SITE_KEY__',
 };
